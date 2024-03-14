8edeadc3b44d38bd84cc3203ffd9024074caba9a
Matias Paz Conti <matiaspazconti2020@gmail.com>
2024-03-14 19:05:01 -0300

    P4-Lab0-2024: Estructura inicial

---
+++ Main.cpp
@@ -0,0 +1,6 @@
+#include "./include/Estudiante.hh"
+#include "./include/ChatGPT.hh"
+#include "./include/Libro.hh"
+#include "./include/PaginaWeb.hh"
+#include "./include/DTFecha.hh"
+#include "./include/DTInfoEstudiante.hh"
---
+++ src/DTFecha.cpp
@@ -0,0 +1,1 @@
+#include "../include/DTFecha.hh"
