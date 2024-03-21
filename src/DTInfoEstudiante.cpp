2d446b98609339913971c766f75f323f95fa216b
Matias Paz Conti <matiaspazconti2020@gmail.com>
2024-03-21 16:28:46 -0300

    DTInfoEstudiante inicializado

--- src/DTInfoEstudiante.cpp
+++ src/DTInfoEstudiante.cpp
@@ -1,1 +1,21 @@
 #include "../include/DTInfoEstudiante.hh"
+
+int DTInfoEstudiante::getCI()
+{
+  return ci;
+}
+int DTInfoEstudiante::getIdentificadorInfo()
+{
+  return identificadorInfo;
+}
+std::string DTInfoEstudiante::getNombreEst()
+{
+  return nombreEst;
+}
+DTInfoEstudiante::DTInfoEstudiante(int c, std::string n, int id)
+{
+  ci = c;
+  nombreEst = n;
+  identificadorInfo = id;
+}
+DTInfoEstudiante::~DTInfoEstudiante() {}
