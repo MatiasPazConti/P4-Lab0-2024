58b76be0a6a66f63f26ee1b887fde22c9ef8565f
Matias Paz Conti <matiaspazconti2020@gmail.com>
2024-03-21 16:49:53 -0300

    Libro & PaginaWeb inicializados

--- src/Libro.cpp
+++ src/Libro.cpp
@@ -1,1 +1,34 @@
 #include "../include/Libro.hh"
+
+std::string Libro::toString() {}
+void Libro::setTitulo(std::string t)
+{
+  titulo = t;
+}
+void Libro::setAutores(std::set<std::string> a)
+{
+  autores = a;
+}
+void Libro::setResumen(std::string r)
+{
+  resumen = r;
+}
+std::string Libro::getTitulo()
+{
+  return titulo;
+}
+std::set<std::string> Libro::getAutores()
+{
+  return autores;
+}
+std::string Libro::getResumen()
+{
+  return resumen;
+}
+Libro::Libro(std::string t, std::set<std::string> a, std::string r)
+{
+  titulo = t;
+  autores = a;
+  resumen = r;
+}
+Libro::~Libro() {}
